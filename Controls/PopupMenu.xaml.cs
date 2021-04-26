8f0eee70e93c5f04abaa73860d5818e3b9fa8f27
Kat21 <49850194+datkat21@users.noreply.github.com>
2021-04-26 07:28:18 -0500

    Organized stuff, new UI, etc

---
+++ Controls/PopupMenu.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SShell.Controls
+{
+    /// <summary>
+    /// Interaction logic for PopupMenu.xaml
+    /// </summary>
+    [ContentProperty("AdditionalContent")]  
+    public partial class PopupMenu : UserControl
+    {
+        public PopupMenu()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public object AdditionalContent
+        {
+            get { return (object)GetValue(AdditionalContentProperty); }
+            set { SetValue(AdditionalContentProperty, value); }
+        }
+        public static readonly DependencyProperty AdditionalContentProperty =
+            DependencyProperty.Register("AdditionalContent", typeof(object), typeof(PopupMenu),
+              new PropertyMetadata(null));
+    }
+
+}
